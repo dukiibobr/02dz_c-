c03bf50fa6141e0f1020d76739de49aae7aafc50
User <User@DESKTOP-7S6KBV1>
2026-08-31 21:16:50 +0300

    Add project files.

---
+++ 02dz_c++/dz.cpp
@@ -0,0 +1,144 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	//int age = 15;
+	//int Age = 55;
+	//int age1 = 5;
+	//int Age_of_man = 65;
+	//int AgeOfMan = 25;
+	//
+
+	//cout << age<<endl;
+	//cout << "age:" << age<<endl ;
+
+	//const float pi = 3.14;//makes value unchangable
+
+
+	//int countHoursInYear;//empty
+	//const int dayIn2000Year = 366;
+	//const int hourInDay = 24;
+	//countHoursInYear = dayIn2000Year * hourInDay;
+	//cout << "hours in 2000s year: " << countHoursInYear << endl;
+
+
+
+	/*float discount = 0.05;
+	int count = 10;
+
+	float cost = 35.99;
+
+	cout << "enter count of products:" << endl;
+	cin >> count;
+	cout << "enter cost:" << endl;
+	cin >> cost;
+
+	float price = count * cost - discount * count * cost;
+
+	cout << "price:" << price << " hrn" << endl;*/
+
+
+	/*int a;
+	cout << "enter number: " << endl;
+	cin >> a;
+	cout << "a= " << a << endl;*/
+
+
+	/*float diagonalD;
+	cout << "enter diagonal in inches:" << endl;
+	cin >> diagonalD;
+
+	const float inch = 2.54;
+	float diagonalCm = inch * diagonalD;
+	cout << "size in cm= " << diagonalCm << endl;*/
+
+
+
+
+
+	//float food;
+	//cout << "how many grams of food your hamster eats in a day?" << endl;
+	//cin >> food;
+
+	//const int gramsInKg = 1000;
+	//const int days = 30;
+	//float foodForDays = food * days / 1000;
+	//cout << "your hamster needs: " << foodForDays << "kg" << endl;
+
+
+
+	//1
+	//float num1;
+	//cout << "enter number1: " << endl;
+	//cin >> num1;
+	//cout << "num1 is: " << num1 << endl;
+
+	//float num2;
+	//cout << "enter number2: " << endl;
+	//cin >> num2;
+	//cout << "num2 is: " << num2 << endl;
+
+	//float num3;
+	//cout << "enter number3: " << endl;
+	//cin >> num3;
+	//cout << "num3 is: " << num3 << endl;
+
+
+	//float suma=num1+num2+num3;
+	//cout <<"suma= " << suma<< endl;
+	
+	
+
+	//2
+	//float num11;
+	//cout << "enter number1: " << endl;
+	//cin >> num11;
+	//
+
+	//float num22;
+	//cout << "enter number2: " << endl;
+	//cin >> num22;
+	//
+
+	//float ser = (num11 + num22) / 2;
+	//cout << "serednye arifmetichne= " << ser << endl;
+
+
+//3
+//float km;
+//cout << "enter kilometres: " << endl;
+//cin >> km;
+//
+//const float meters = 1000;
+//
+//float distanceMeters = km * meters;
+//cout << "your km in m is: " << distanceMeters << endl;
+
+
+
+
+int chipsP = 50;
+int chipsQ = 2;
+cout << "i bought " << chipsQ << "bags of chips,with price of " << chipsP << endl;
+
+int fishP = 500;
+int fishQ = 3;
+cout << "i bought " << fishQ << "of fish,with price of " << fishP << endl;
+
+int pineappleP = 250;
+int pineappleQ = 5;
+cout << "i bought " << pineappleQ << "of pineapple,with price of " << pineappleP << endl;
+
+int pastaP = 300;
+int pastaQ = 7;
+cout << "i bought " << pastaQ << "of pasta with price of " << pastaP << endl;
+
+
+
+float sumaPurchase = (chipsP * chipsQ) + (fishP * fishQ) + (pineappleP * pineappleQ) + (pastaP * pastaQ);
+cout << "summ of purchase= " << sumaPurchase << endl;
+}
